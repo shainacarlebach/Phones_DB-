b06e5950879b4eb235886e28a49e420d063aaf91
Shaina Carlebach <schiffs@aisj.co.il>
2017-10-20 08:41:55 +0200

    changeset

--- back/models/phoneModel.php
+++ back/models/phoneModel.php
@@ -3,17 +3,18 @@ require_once'models.php';
 require_once "C:/xampp/htdocs/Phones_DB/back/common/DAL.php";
 
 Class PhoneModel extends Model implements JsonSerializable{
- public $id_phones;
- public $model_phones;
- public $manu_code;
+ private $id_phones;
+ private $model_phones;
+ private $manu_code;
+private $image;
 
-
- function __construct($id_phones, $model_phones,$manu_code){
+ function __construct($id_phones, $model_phones,$manu_code,$image){
  
      $this->tableName ='phones_table';
      $this->id_phones-$id_phones;
      $this->model_phones=$model_phones;
       $this->manu_code=$manu_code; 
+      $this->image=$image;
  }
 
  
@@ -28,11 +29,16 @@ function getModel(){
 function getCode(){
     return $this->manu_code;
 }
+ public function getImage(){
+        return $this->image;
+            }
+
 function jsonSerialize(){
     return[
        "$id_phones"=>$this->getID(),
        "$model_phones"=>$this->getModel(),
-       "$manu_code"=>$this->getCode()
+       "$manu_code"=>$this->getCode(),
+       "image_name"=>$this->getImage()
            ];
 }
 }
